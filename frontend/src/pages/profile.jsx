5cafd59cb5a7aa945e6e504eda1f2e1312147379
kajsa99 <kajsa.palmen@gmail.com>
2025-10-30 15:08:38 +0100

    adding profile page content, needs database

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -2,9 +2,21 @@ export default function Profile() {
     return (
         <div className="flex flex-col p-10 bg-stone-500 text-white min-h-screen">
             <h1 className="flex m-4 text-xl justify-center">User Profile</h1>
-            <p className="flex m-4 text-center justify-center">
-                This is the profile page.
-            </p>
+            <div className="bg-stone-200 rounded-lg text-black">
+                <h2 className="flex m-4 text-center justify-center">
+                    profile.name
+                </h2>
+                <p className="flex m-4 text-center justify-center">
+                    Email: profile.email
+                </p>
+                <p className="flex m-4 text-center justify-center">
+                    Password:
+                    <button>Change password</button>
+                </p>
+                <p className="flex m-4 text-center justify-center">
+                    Member since: profile.memberSince
+                </p>
+            </div>
         </div>
     );
 }
