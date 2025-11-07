79f53ec4c4a20deda13fdd697ac574de310b977a
kajsa99 <kajsa.palmen@gmail.com>
2025-11-07 12:14:44 +0100

    styled smultroncard component and deleted settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,8 @@ import { Navbar } from "../components/navbar.jsx";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import New from "./pages/new.jsx";
-import Profiles from "./pages/profiles.jsx";
-import ProfileDetail from "./pages/profiledetail.jsx";
-import Settings from "./pages/settings.jsx";
+import Profiles from "./pages/Profiles.jsx";
+import ProfileDetail from "./pages/ProfileDetail.jsx";
 import Login from "./pages/login.jsx";
 import Signup from "./pages/signup.jsx";
 import { Footer } from "../components/footer.jsx";
@@ -21,7 +20,6 @@ function App() {
                 <Route path="/new" element={<New />} />
                 <Route path="/profile" element={<Profiles />} />
                 <Route path="/profile/:id" element={<ProfileDetail />} />
-                <Route path="/settings" element={<Settings />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
             </Routes>
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,30 +1,12 @@
-import { useEffect, useState } from "react";
+import { SmultronCard } from "../../components/smultroncard";
 
 export default function Home() {
-    const [cards, setCards] = useState([]);
-    useEffect(() => {
-        fetch("/api/smultron")
-            .then((res) => res.json())
-            .then(setCards)
-            .catch(console.error);
-    }, []);
     return (
         <div className="flex flex-col p-10 bg-stone-500 text-white min-h-screen">
             <h1 className="flex m-4 text-xl justify-center">
                 Explore smultron
             </h1>
-
-            <ul>
-                {cards.map((s) => (
-                    <li key={s.id}>
-                        {s.title}
-                        {s.description}
-                        {s.location}
-                        {s.image_url}
-                        {s.created_at}
-                    </li>
-                ))}
-            </ul>
+            <SmultronCard />
         </div>
     );
 }
