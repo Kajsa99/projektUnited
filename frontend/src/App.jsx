40c6b7bef8d26be7155acd9f769cecbee3d27c1d
mapel420 <max.asensio13@gmail.com>
2025-11-16 23:33:26 +0100

    add web optimization lazy loading +suspense

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,34 @@
 import "./App.css";
 import { Navbar } from "../components/navbar.jsx";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home.jsx";
-import New from "./pages/new.jsx";
-import ProfileDetail from "./pages/profiledetail.jsx";
-import Login from "./pages/login.jsx";
-import Signup from "./pages/signup.jsx";
 import { Footer } from "../components/footer.jsx";
+import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./pages/home.jsx"));
+const New = lazy(() => import("./pages/new.jsx"));
+const ProfileDetail = lazy(() => import("./pages/profiledetail.jsx"));
+const Login = lazy(() => import("./pages/login.jsx"));
+const Signup = lazy(() => import("./pages/signup.jsx"));
 
 function App() {
-    console.log(" App component rendered");
+  console.log(" App component rendered");
 
-    return (
-        <div>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/new" element={<New />} />
-                <Route path="/profile/:id" element={<ProfileDetail />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-            </Routes>
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/profile/:id" element={<ProfileDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Suspense>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
